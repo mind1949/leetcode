dda7c69c4a15c2d0665e55cb6becdd30a59c565f
mind1949 <lianjie1949@gmail.com>
2020-07-01 10:33:11 +0800

    fix typo

--- algs/0017.letter_combinations_of_phone_number.go
+++ algs/0017.letter_combinations_of_phone_number.go
@@ -1,7 +1,7 @@
 package algs
 
 /*
-letterCombinations sloves the following problem:
+LetterCombinations sloves the following problem:
 Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
 
 A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
