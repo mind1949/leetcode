d9a30bfa323a9d9db0d576f8ea1deec3b77497aa
mind1949 <lianjie1949@gmail.com>
2020-07-08 16:00:35 +0800

    complete problem: 0028.implement_strstr

---
+++ algs/0028.implement_strstr.go
@@ -0,0 +1,44 @@
+package algs
+
+/*
+StrStr solves the following problem:
+	Implement strStr().
+
+	Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+	Example 1:
+
+		Input: haystack = "hello", needle = "ll"
+		Output: 2
+
+	Example 2:
+
+		Input: haystack = "aaaaa", needle = "bba"
+		Output: -1
+
+	Clarification:
+
+	What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+	For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+*/
+func StrStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(haystack) < len(needle) {
+		return -1
+	}
+
+	head, tail := 0, len(needle)-1
+	for tail < len(haystack) {
+		if haystack[head:tail+1] == needle {
+			return head
+		}
+
+		head++
+		tail++
+	}
+
+	return -1
+}
---
+++ algs/0028.implement_strstr_test.go
@@ -0,0 +1,23 @@
+package algs
+
+import (
+	"testing"
+)
+
+func TestStrStr(t *testing.T) {
+	for _, c := range []struct {
+		haystack string
+		needle   string
+		expect   int
+	}{
+		{"hello", "ll", 2},
+		{"aaaaa", "bba", -1},
+		{"", "", 0},
+	} {
+		got := StrStr(c.haystack, c.needle)
+		if got != c.expect {
+			format := "haystack: %s\t|\tneedle: %s\t|\texpect: %d\t|\tgot: %d"
+			t.Errorf(format, c.haystack, c.needle, c.expect, got)
+		}
+	}
+}
