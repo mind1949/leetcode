77ede9526903a0fad88f909f2d04925c99fc23ae
mind1949 <lianjie1949@gmail.com>
2020-07-08 23:28:40 +0800

    use rabin-karp alg to solve problem: 0028.implement_strstr

--- algs/0028.implement_strstr.go
+++ algs/0028.implement_strstr.go
@@ -1,5 +1,7 @@
 package algs
 
+import "math"
+
 /*
 StrStr solves the following problem:
 	Implement strStr().
@@ -35,6 +37,48 @@ func StrStr(haystack string, needle string) int {
 
 		head++
 		tail++
+	}
+
+	return -1
+}
+
+// StrStrRabinKarp 使用rabin-karp算法实现的StrStr
+func StrStrRabinKarp(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	if len(needle) > len(haystack) {
+		return -1
+	}
+
+	// 哈希算法
+	h2Hash := func(i int) int {
+		return int(haystack[i] - 'a')
+	}
+	n2Hash := func(i int) int {
+		return int(needle[i] - 'a')
+	}
+
+	const (
+		base   = 26
+		module = 2 << 30
+	)
+	var hhash, nhash int
+	for i := 0; i < len(needle); i++ {
+		hhash = (hhash*base + h2Hash(i)) % module
+		nhash = (nhash*base + n2Hash(i)) % module
+	}
+	if hhash == nhash {
+		return 0
+	}
+
+	sub := int(math.Pow(float64(base), float64(len(needle)))) % module
+	// 滚动哈希，判断字符串是否相等
+	for i := 1; i < len(haystack)-len(needle)+1; i++ {
+		hhash = (hhash*base - h2Hash(i-1)*sub + h2Hash(i+len(needle)-1)) % module
+		if hhash == nhash {
+			return i
+		}
 	}
 
 	return -1
--- algs/0028.implement_strstr_test.go
+++ algs/0028.implement_strstr_test.go
@@ -13,8 +13,28 @@ func TestStrStr(t *testing.T) {
 		{"hello", "ll", 2},
 		{"aaaaa", "bba", -1},
 		{"", "", 0},
+		{"aabaaabaaac", "aabaaac", 4},
 	} {
 		got := StrStr(c.haystack, c.needle)
+		if got != c.expect {
+			format := "haystack: %s\t|\tneedle: %s\t|\texpect: %d\t|\tgot: %d"
+			t.Errorf(format, c.haystack, c.needle, c.expect, got)
+		}
+	}
+}
+
+func TestStrStrRabinKarp(t *testing.T) {
+	for _, c := range []struct {
+		haystack string
+		needle   string
+		expect   int
+	}{
+		{"hello", "ll", 2},
+		{"aaaaa", "bba", -1},
+		{"", "", 0},
+		{"aabaaabaaac", "aabaaac", 4},
+	} {
+		got := StrStrRabinKarp(c.haystack, c.needle)
 		if got != c.expect {
 			format := "haystack: %s\t|\tneedle: %s\t|\texpect: %d\t|\tgot: %d"
 			t.Errorf(format, c.haystack, c.needle, c.expect, got)
