638a86b2aecd1eb70d2750b99fe47578709ebfd6
mind1949 <lianjie1949@gmail.com>
2020-07-08 16:05:56 +0800

    refactor: 0028.implement_strstr

--- algs/0028.implement_strstr.go
+++ algs/0028.implement_strstr.go
@@ -26,9 +26,6 @@ func StrStr(haystack string, needle string) int {
 	if len(needle) == 0 {
 		return 0
 	}
-	if len(haystack) < len(needle) {
-		return -1
-	}
 
 	head, tail := 0, len(needle)-1
 	for tail < len(haystack) {
