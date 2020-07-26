3efaa9892586cd3394e166c4edd08a13c9ac49bc
mind1949 <lianjie1949@gmail.com>
2020-07-26 22:08:58 +0800

    using eudipleural method to solve problem: 0032.longest_valid_parentheses

--- algs/0032.longest_valid_parentheses.go
+++ algs/0032.longest_valid_parentheses.go
@@ -70,3 +70,44 @@ func LongestValidParentheses(s string) int {
 
 	return max
 }
+
+// LongestValidParentheses1 左右迭代对称求值
+func LongestValidParentheses1(s string) int {
+	var (
+		l, r, maxLen int
+	)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			l++
+		} else {
+			r++
+		}
+		if l == r {
+			maxLen = max(maxLen, 2*r)
+		} else if r > l {
+			l, r = 0, 0
+		}
+	}
+
+	r, l = 0, 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ')' {
+			r++
+		} else {
+			l++
+		}
+		if l == r {
+			maxLen = max(maxLen, 2*l)
+		} else if l > r {
+			l, r = 0, 0
+		}
+	}
+	return maxLen
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
--- algs/0032.longest_valid_parentheses_test.go
+++ algs/0032.longest_valid_parentheses_test.go
@@ -2,19 +2,30 @@ package algs
 
 import "testing"
 
+var cases0032 = []struct {
+	s      string
+	expect int
+}{
+	{"(()", 2},
+	{")()())", 4},
+	{"", 0},
+	{"((((", 0},
+	{"))))", 0},
+	{")))(((", 0},
+}
+
 func TestLongestValidParentheses(t *testing.T) {
-	for _, c := range []struct {
-		s      string
-		expect int
-	}{
-		{"(()", 2},
-		{")()())", 4},
-		{"", 0},
-		{"((((", 0},
-		{"))))", 0},
-		{")))(((", 0},
-	} {
+	for _, c := range cases0032 {
 		got := LongestValidParentheses(c.s)
+		if got != c.expect {
+			t.Errorf("s: %q\t|\texpect: %d\t|\tgot: %d", c.s, c.expect, got)
+		}
+	}
+}
+
+func TestLongestValidParenthesesSymmetry(t *testing.T) {
+	for _, c := range cases0032 {
+		got := LongestValidParentheses1(c.s)
 		if got != c.expect {
 			t.Errorf("s: %q\t|\texpect: %d\t|\tgot: %d", c.s, c.expect, got)
 		}
