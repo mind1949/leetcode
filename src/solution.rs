9dd78007a482eac74a93148c02cd2e6f6644df4b
mind1949 <lianjie1949@gmail.com>
2024-07-13 17:56:22 +0800

    add rust solution to group anagrams

--- src/solution.rs
+++ src/solution.rs
@@ -1,6 +1,8 @@
 /// [Leetcode](https://leetcode.cn) solution
 pub struct Solution;
 
+use std::collections::HashMap;
+
 impl Solution {
     /// [1. Tow Sum](https://leetcode.cn/problems/two-sum/description)
     ///
@@ -40,7 +42,7 @@ impl Solution {
     ///  
     /// Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut indices = std::collections::HashMap::new();
+        let mut indices = HashMap::new();
         for (i, v) in nums.iter().enumerate() {
             if let Some(&idx) = indices.get(&(target - v)) {
                 return vec![i as i32, idx as i32];
@@ -48,5 +50,55 @@ impl Solution {
             indices.insert(v, i);
         }
         return vec![];
+    }
+
+    /// 49. [Group Anagrams](https://leetcode.cn/problems/group-anagrams/description/)
+    ///
+    /// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+    ///
+    /// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+    ///
+    ///  
+    ///
+    /// Example 1:
+    ///
+    /// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+    /// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+    ///
+    /// Example 2:
+    ///
+    /// Input: strs = [""]
+    /// Output: [[""]]
+    ///
+    /// Example 3:
+    ///
+    /// Input: strs = ["a"]
+    /// Output: [["a"]]
+    ///
+    ///  
+    ///
+    /// Constraints:
+    ///
+    ///     1 <= strs.length <= 104
+    ///     0 <= strs[i].length <= 100
+    ///     strs[i] consists of lowercase English letters.
+    ///
+    ///
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut matchers = HashMap::new();
+        for str in strs.iter() {
+            let mut matcher = [0; 26];
+            for e in str.chars() {
+                matcher[e as usize - 'a' as usize] += 1;
+            }
+            let group = matchers.entry(matcher).or_insert(vec![]);
+            group.push(str.to_string());
+        }
+
+        let mut result: Vec<Vec<String>> = Vec::with_capacity(matchers.len());
+        for (_, group) in matchers {
+            result.push(group);
+        }
+        return result;
     }
 }
