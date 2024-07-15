1d5bd937775717364503a4626154052e76f0332f
mind1949 <lianjie1949@gmail.com>
2024-07-15 22:50:41 +0800

    solve container with most water with rustlang

--- src/solution.rs
+++ src/solution.rs
@@ -182,4 +182,66 @@ impl Solution {
             }
         }
     }
+
+    /// [11. Container with most water](https://leetcode.cn/problems/container-with-most-water/description/)
+    ///
+    /// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+    ///
+    /// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+    ///
+    /// Return the maximum amount of water a container can store.
+    ///
+    /// Notice that you may not slant the container.
+    ///
+    ///  
+    ///
+    /// Example 1:
+    ///
+    ///  8|                    |                |              
+    ///  7|                    |                |              |
+    ///  6|          |         |          |     |              |
+    ///  5|          |         |          |     |       |      |
+    ///  4|          |         |          |     |       |      |
+    ///  3|          |         |          |     |       |      |
+    ///  2|          |         |    |     |     |       |      |
+    ///  1|    |     |         |    |     |     |       |      |
+    ///   -------------------------------------------------------
+    ///   0    1     2         3    4     5     6       7      8
+    ///
+    /// Input: height = [1,8,6,2,5,4,8,3,7]
+    /// Output: 49
+    /// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+    ///
+    /// Example 2:
+    ///
+    /// Input: height = [1,1]
+    /// Output: 1
+    ///
+    ///  
+    ///
+    /// Constraints:
+    ///
+    ///     n == height.length
+    ///     2 <= n <= 105
+    ///     0 <= height[i] <= 104
+    ///
+    ///
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut i = 0;
+        let mut j = height.len() - 1;
+        let mut max_area = 0;
+
+        while j > i {
+            if height[i] > height[j] {
+                let area = height[j] * (j - i) as i32;
+                max_area = max_area.max(area);
+                j -= 1;
+            } else {
+                let area = height[i] * (j - i) as i32;
+                max_area = max_area.max(area);
+                i += 1;
+            }
+        }
+        return max_area;
+    }
 }
