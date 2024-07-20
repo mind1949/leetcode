55f826c07f62d07026905f0d85c0e2ac31078a9b
mind1949 <lianjie1949@gmail.com>
2024-07-20 08:56:05 +0800

    solve find_all_anagrams_in_a_string with rustlang

--- src/solution.rs
+++ src/solution.rs
@@ -429,4 +429,72 @@ impl Solution {
         }
         return longest as i32;
     }
+
+    /// [438. Find All Anagrams in a String](https://leetcode.cn/problems/find-all-anagrams-in-a-string/description/)
+    ///
+    /// Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+    ///
+    /// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+    ///
+    ///  
+    ///
+    /// Example 1:
+    ///
+    /// Input: s = "cbaebabacd", p = "abc"
+    /// Output: [0,6]
+    /// Explanation:
+    /// The substring with start index = 0 is "cba", which is an anagram of "abc".
+    /// The substring with start index = 6 is "bac", which is an anagram of "abc".
+    ///
+    /// Example 2:
+    ///
+    /// Input: s = "abab", p = "ab"
+    /// Output: [0,1,2]
+    /// Explanation:
+    /// The substring with start index = 0 is "ab", which is an anagram of "ab".
+    /// The substring with start index = 1 is "ba", which is an anagram of "ab".
+    /// The substring with start index = 2 is "ab", which is an anagram of "ab".
+    ///
+    ///  
+    ///
+    /// Constraints:
+    ///
+    ///     1 <= s.length, p.length <= 3 * 104
+    ///     s and p consist of lowercase English letters.
+    pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
+        if s.len() < p.len() {
+            return vec![];
+        }
+
+        let chars_p: Vec<char> = p.chars().collect();
+        let mut count_p = [0; 26];
+        for &c in &chars_p {
+            count_p[c as usize - 'a' as usize] += 1;
+        }
+
+        let chars_s: Vec<char> = s.chars().collect();
+        let mut count_sub = [0; 26];
+        for &c in chars_s[0..chars_p.len()].iter() {
+            count_sub[c as usize - 'a' as usize] += 1;
+        }
+
+        let mut res = vec![];
+        let (mut i, mut j) = (0, p.len() - 1);
+        while i <= j && j < chars_s.len() {
+            if count_p == count_sub {
+                res.push(i as i32);
+            }
+
+            if let Some(&c) = chars_s.get(i) {
+                count_sub[c as usize - 'a' as usize] -= 1;
+            }
+            i += 1;
+            j += 1;
+            if let Some(&c) = chars_s.get(j) {
+                count_sub[c as usize - 'a' as usize] += 1;
+            }
+        }
+
+        return res;
+    }
 }
