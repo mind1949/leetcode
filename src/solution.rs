87da8703f9cf9a8783b7508d2bc5df3aa37738f2
mind1949 <lianjie1949@gmail.com>
2024-07-20 14:02:58 +0800

    solve subarray_sum_equals_k

--- src/solution.rs
+++ src/solution.rs
@@ -497,4 +497,40 @@ impl Solution {
 
         return res;
     }
+
+    /// [560. Subarray Sum Equal K](https://leetcode.cn/problems/subarray-sum-equals-k/description/)
+    ///
+    /// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+    ///
+    /// A subarray is a contiguous non-empty sequence of elements within an array.
+    ///
+    /// Example 1:
+    ///
+    /// Input: nums = [1,1,1], k = 2
+    /// Output: 2
+    ///
+    /// Example 2:
+    ///
+    /// Input: nums = [1,2,3], k = 3
+    /// Output: 2
+    ///
+    /// Constraints:
+    ///
+    ///	1 <= nums.length <= 2 * 10^4
+    ///	-1000 <= nums[i] <= 1000
+    ///	-10^7 <= k <= 10^7
+    pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
+        let mut res = 0;
+        let mut m = HashMap::from([(0, 1)]);
+        let (mut sum, mut i) = (0, 0);
+        while i < nums.len() {
+            sum += nums[i];
+            if let Some(count) = m.get(&(sum - k)) {
+                res += count;
+            }
+            m.entry(sum).and_modify(|count| *count += 1).or_insert(1);
+            i += 1;
+        }
+        return res;
+    }
 }
