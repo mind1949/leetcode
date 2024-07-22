e1e1377ee574ccaa287d151efcce53def3d80384
mind1949 <lianjie1949@gmail.com>
2024-07-22 20:24:03 +0800

    solve maximum_subarray

--- src/solution.rs
+++ src/solution.rs
@@ -679,4 +679,60 @@ impl Solution {
 
         return s[l..r].to_string();
     }
+
+    /// [53. maximum subarray](https://leetcode.cn/problems/maximum-subarray/description)
+    ///
+    /// Given an integer array nums, find the
+    /// subarray
+    /// with the largest sum, and return its sum.
+    ///
+    ///  
+    ///
+    /// Example 1:
+    ///
+    /// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    /// Output: 6
+    /// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+    ///
+    /// Example 2:
+    ///
+    /// Input: nums = [1]
+    /// Output: 1
+    /// Explanation: The subarray [1] has the largest sum 1.
+    ///
+    /// Example 3:
+    ///
+    /// Input: nums = [5,4,-1,7,8]
+    /// Output: 23
+    /// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+    ///
+    ///  
+    ///
+    /// Constraints:
+    ///
+    ///     1 <= nums.length <= 10^5
+    ///     -10^4 <= nums[i] <= 10^4
+    ///
+    ///  
+    ///
+    /// Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+    pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        if nums.len() == 0 {
+            return 0;
+        }
+        // 状态转移方程:
+        // f(i) = max(f(i-1), nums[i])
+        //
+        // 初始状态:
+        // f(0) = nums[0]
+
+        // dp dp 表表示 f(i-1) 的值
+        let mut dp = nums[0];
+        let mut res = dp;
+        for i in 1..nums.len() {
+            dp = std::cmp::max(nums[i], dp + nums[i]);
+            res = std::cmp::max(res, dp);
+        }
+        res
+    }
 }
