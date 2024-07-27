cc7ce3dd74fa54dadb58ed1cb5460dec1b67453b
mind1949 <lianjie1949@gmail.com>
2024-07-27 15:27:43 +0800

    solve first missing positive

--- src/solution.rs
+++ src/solution.rs
@@ -873,4 +873,63 @@ impl Solution {
         }
         result
     }
+
+    /// [41. Frist Missing Positive]
+    ///
+    /// Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+    ///
+    /// You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+    ///
+    /// Example 1:
+    ///
+    /// Input: nums = [1,2,0]
+    /// Output: 3
+    /// Explanation: The numbers in the range [1,2] are all in the array.
+    ///
+    /// Example 2:
+    ///
+    /// Input: nums = [3,4,-1,1]
+    /// Output: 2
+    /// Explanation: 1 is in the array but 2 is missing.
+    ///
+    /// Example 3:
+    ///
+    /// Input: nums = [7,8,9,11,12]
+    /// Output: 1
+    /// Explanation: The smallest positive integer 1 is missing.
+    ///
+    /// Constraints:
+    ///
+    ///	1 <= nums.length <= 10^5
+    ///	-2^31 <= nums[i] <= 2^31 - 1
+    pub fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
+        for i in 0..nums.len() {
+            if nums[i] == 0 {
+                nums[i] = -1;
+            }
+        }
+
+        for i in 0..nums.len() {
+            while nums[i] >= 1 && nums[i] <= nums.len() as i32 {
+                let j = nums[i] as usize - 1;
+                let tmp = nums[j];
+                nums[j] = 0;
+                if j == i {
+                    break;
+                }
+                if tmp != 0 {
+                    nums[i] = tmp;
+                } else {
+                    nums[i] = -1;
+                }
+            }
+        }
+
+        for (i, &num) in nums.iter().enumerate() {
+            if num != 0 {
+                return i as i32 + 1;
+            }
+        }
+        nums.len() as i32 + 1
+    }
 }
