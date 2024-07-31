49163d56a5c4113617692b8de5a1898c8168fcb4
mind1949 <lianjie1949@gmail.com>
2024-07-31 23:25:46 +0800

    solve rotate_image with rustlang

--- src/solution.rs
+++ src/solution.rs
@@ -1064,4 +1064,47 @@ impl Solution {
         }
         res
     }
+
+    /// [48. Rotate Image](https://leetcode.cn/problems/rotate-image/description)
+    ///
+    /// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+    ///
+    /// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+    ///
+    /// Example 1:
+    ///
+    /// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    /// Output: [[7,4,1],[8,5,2],[9,6,3]]
+    ///
+    /// Example 2:
+    ///
+    /// Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    /// Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+    ///
+    /// Constraints:
+    ///
+    ///	n == matrix.length == matrix[i].length
+    ///	1 <= n <= 20
+    ///	-1000 <= matrix[i][j] <= 1000
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        if matrix.is_empty() {
+            return;
+        }
+
+        let n = matrix.len();
+        for l in 0..(n / 2 + n % 2) {
+            for r in (l + 1..n - l).rev() {
+                let (mut nr, mut nl) = (l, n - 1 - r);
+                let mut tmp = matrix[nr][nl];
+                matrix[nr][nl] = matrix[r][l];
+                loop {
+                    (nr, nl) = (nl, n - 1 - nr);
+                    (tmp, matrix[nr][nl]) = (matrix[nr][nl], tmp);
+                    if nr == r && nl == l {
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
